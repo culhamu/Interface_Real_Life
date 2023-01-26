0e5ba7842fed87d52d92c47dd9b2354cc665b2d6
Mustafa Culha <merguven753@gmail.com>
2023-01-26 21:05:49 +0300

    Basit Problemler

---
+++ src/problemler/Main.java
@@ -0,0 +1,11 @@
+package problemler;
+
+public class Main {
+    //Basit Matematik ve Fizik problemleri
+    //Dairenin alani
+    //Ucgenin cevresi
+    //3 boyutlu Vektorun ic carpimi
+    public static void main(String[] args) {
+
+    }
+}
